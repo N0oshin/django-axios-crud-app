d7136dae4f4880166380dc6d2059af6e8266d200
N0oshin <noosh1771@gmail.com>
2025-06-27 13:44:02 +0400

    Upgraded to ModelViewSet+Built a frontend with HTML,JS,Axios

--- myproject/api/urls.py
+++ myproject/api/urls.py
@@ -1,6 +1,28 @@
+from rest_framework.routers import DefaultRouter
+from .views import ItemViewSet, index_view
 from django.urls import path
-from .views import ItemsView
 
-urlpatterns = [
-    path("items/", ItemsView.as_view(), name="items"),
+router = DefaultRouter()
+router.register(r"items", ItemViewSet)  # r'items' makes the endpoint /items/
+
+urlpatterns = router.urls + [
+    path("app/", index_view, name="index"),  # http://127.0.0.1:8000/api/app/
 ]
+
+
+"""
+DRF auto-creates routes like:
+- GET /items/ → list all items
+- POST /items/ → create new item
+- GET /items/{id}/ → retrieve a specific item
+- PUT /items/{id}/ → update
+- DELETE /items/{id}/ → delete
+
+"""
+
+# from django.urls import path
+# from .views import ItemsView
+
+# urlpatterns = [
+#     path("items/", ItemsView.as_view(), name="items"),
+# ]
--- myproject/api/views.py
+++ myproject/api/views.py
@@ -1,10 +1,22 @@
 # Views in Django are responsible for handling the endpoint logic, they take the request, perform some operations eg. load model from database, serialize it into JSON format and return the HTTP Response.
 
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import viewsets
 from .models import item
 from .serializers import ItemSerializer
 
+
+# - handle the GET and POST operations for the Item model.
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = item.objects.all()
+    serializer_class = ItemSerializer
+
+
+""" 
+in this method we have define each method (e.g. get, post) manually but More control and customization
+You define URLs explicitly via urls.py and path() or re_path()
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 class ItemsView(APIView):
     def get(self, request):
@@ -18,3 +30,9 @@ class ItemsView(APIView):
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
+"""
+from django.shortcuts import render
+
+
+def index_view(request):
+    return render(request, "index.html")
